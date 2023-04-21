044b45976440fd40e3e2502f19f0923170794eaf
Cleiver <creibis10@gmail.com>
2023-04-21 15:05:31 -0300

    get transacoes com validacao de token implementada

--- src/app.js
+++ src/app.js
@@ -116,15 +116,15 @@ app.post('/nova-transacao/:tipo', async (req, res) => {
     const {descricao, valor} = req.body;
     const {tipo} = req.params;
 
-    const existeToken = await db.collection('sessoes').findOne({token: userToken})
-    if(!existeToken) return res.status(401).send('usuario nao pode fazer requisicao')
+    const existeToken = await db.collection('sessoes').findOne({token: userToken});
+    if(!existeToken) return res.status(401).send('usuario nao pode fazer requisicao');
 
     const usuario = existeToken.idUsuario;
 
     const useSchemaParametros = joi.object({
       userToken: joi.string().required(),
       valor: joi.number().precision(2).required(),
-      tipo: joi.string().required(),
+      tipo: joi.string().valid("entrada", "saida").required(),
       descricao: joi.string().required()
     });
 
@@ -138,8 +138,8 @@ app.post('/nova-transacao/:tipo', async (req, res) => {
       const dia = dayjs().format('DD/MM')
       const transacaoDB = {descricao, tipo, valor, usuario, dia}
 
-      await db.collection('transacoes').insertOne(transacaoDB)
-      res.status(200).send("transacao feita com sucesso")
+      await db.collection('transacoes').insertOne(transacaoDB);
+      res.status(200).send("transacao feita com sucesso");
       
     } catch (error) {
       console.error(error);
@@ -147,84 +147,21 @@ app.post('/nova-transacao/:tipo', async (req, res) => {
     }
   });
 
-// app.post('/messages', async (req, res) => {
-//   const message = req.body;
-//   const {to, text, type} = req.body;
+  app.get('/home', async (req, res) => {
 
-//   let from;
+    const userToken = req.headers.authorization?.replace("Bearer ", "");
+    const validaToken = await db.collection('sessoes').findOne({token: userToken});
+    if(!validaToken) return res.status(401).send("token invalido");
 
-//   if(req.headers.user === undefined){
-//     from = req.headers.User
-//   }else{
-//     from = req.headers.user
-//   }
+    try {
+      const transacoes = await db.collection('transacoes').find({usuario: validaToken.idUsuario}).toArray();
 
-//   const userSchema = joi.object({
-//     to: joi.string().required(),
-//     text: joi.string().required(),
-//     type: joi.string().valid("message", "private_message").required()
-//   })
-
-//   const validacao = userSchema.validate(message, {abortEarly: false})
-//   if (validacao.error) {
-//     const errors = validacao.error.details.map((detail) => detail.message);
-//     return res.status(422).send(errors);
-//   }
-//   const novaMensagem = {
-//     from,
-//     to, 
-//     text,
-//     type,
-//     time: dayjs().format('HH:mm:ss')
-//   }
-
-//   try {
-
-//     const usuarioOnline = await db.collection('participants').findOne({name: from})
-//     console.log(usuarioOnline)
-//     if(!usuarioOnline) return res.sendStatus(422)
-
-//     await db.collection("messages").insertOne(novaMensagem)
-//     console.log("mensagem enviada com sucesso");
-//     res.sendStatus(201);
-//   } catch (error) {
-//     console.error(error);
-//     res.sendStatus(500);
-//   }
-// });
-
-// app.post('/status', async (req, res) => {
-//   const user = req.headers.user
-
-//   const userSchema = joi.object({
-//     user: joi.string().required()
-//   })
-
-//   // validacao de user usando joi
-//   const validacao = userSchema.validate({user}, {abortEarly: false})
-//   if(validacao.error) {
-//     return res.status(404).send(validacao.error.details);
-//   }
-
-//   // verificar se o usuario consta na lista atual de participants
-//   const existeNome = await db.collection('participants').findOne({name: user});
-//   // se nao existir retorna nulo e nao entra no if
-//   if(!existeNome) return res.status(404).send("usuario nao existe");
-
-//   try {
-
-//     const atualizacao = await db.collection('participants').updateOne(
-//       {name: user}, 
-//       {$set: {lastStatus: Date.now()}}
-//     )
-//     if(atualizacao.modifiedCount === 0) return res.status(404).send("nao foi possivel atualizar status")
-
-//     res.status(200).send("last status atualizado com sucesso")
-//   } catch (error) {
-//     console.error(error);
-//     res.sendStatus(500);
-//   }
-// });
+      res.send(transacoes);
+    } catch (error) {
+      console.error(error);
+      res.sendStatus(500);
+    }
+  });
 
 // app.delete('/messages/:id', async (req, res) => {
 //   const idMessage = req.params.id;
